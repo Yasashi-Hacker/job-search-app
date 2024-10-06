1ea7d387408dd8717363e2c3f2cc67f70c487675
Yasashi-Hacker <jinsoku591@gmail.com>
2024-10-06 21:12:07 +0900

    Connected Django project to PostgreSQL database

--- job_search_app/settings.py
+++ job_search_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'job_search_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'job_search_db',
+        'USER': 'Y4s4shi_H4ck3r',
+        'PASSWORD': 'melonsoda',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
