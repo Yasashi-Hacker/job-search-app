caa7bea1597029ea1faa703b4dc5d4406fac4cba
Yasashi-Hacker <jinsoku591@gmail.com>
2024-10-07 17:09:56 +0900

    Define users and job_listings models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class jobs(models.Model):
+    job_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=161)
+    job_title = models.CharField(max_length=100)
+    salary = models.IntegerField
+    location = models.CharField(max_length=200)
+    qualification = models.CharField(max_length=500)
+    job_description = models.CharField(max_length=500)
+    process = models.CharField(max_length=500)
+    company_profile = models.CharField(max_length=500)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    user_id = models.CharField(max_length=30, primary_key=True)
+    password = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=30)
+    final_school = models.CharField(max_length=90)
+    major = models.CharField(max_length=90)
+    degree = models.CharField(max_length=4)
